7d78f3d1c42a2fbf1e44ad1af53c556bf59f786c
Fábio Pimentel <fabiolp4@gmail.com>
2018-02-26 00:25:44 -0300

    Tarefa 05

---
+++ tarefas/tarefa-05/README.md
@@ -0,0 +1,19 @@
+# Tarefa 004
+
+14bis vs 100 Meteóros
+---
+
+O objetivo é destruir 100 meteoros antes de morrer.
+Os comandos usados no jogo são:
+
+1. A/W/S/D -> Mover o avião
+2. Espaço -> Atirar
+
+Telas
+----
+![Vencendo](https://github.com/fabiopimentel/EDL/blob/master/tarefas/tarefa-04/screenshots/14bis_vencedor.png)   
+
+![Perdendo](https://github.com/fabiopimentel/EDL/blob/master/tarefas/tarefa-04/screenshots/14bis_destruido.png)   
+
+![Jogando](https://github.com/fabiopimentel/EDL/blob/master/tarefas/tarefa-04/screenshots/game_executando.gif)   
+
---
+++ tarefas/tarefa-05/main.lua
@@ -0,0 +1,317 @@
+LARGURA_TELA = 320
+
+------------------------------------------------------------
+-- NOME: LARGURA_TELA
+-- PROPRIEDADE: valor
+-- TEMPO: compilação
+-- EXPLICAÇÃO: o valor de LARGURA_TELA será definido na 
+--compilação por ela ser global 
+------------------------------------------------------------ 	
+
+
+ALTURA_TELA = 480
+
+------------------------------------------------------------
+-- NOME: ALTURA_TELA
+-- PROPRIEDADE: tipo
+-- TEMPO: compilação
+-- EXPLICAÇÃO: Como ALTURA_TELA é uma global e não há uma
+-- definição explícita do tipo da variável temos que isso é
+-- descoberto em tempo de compilação
+------------------------------------------------------------
+
+MAX_METEOROS_TELA = 12
+FIM_JOGO=false
+
+------------------------------------------------------------
+-- NOME: false
+-- PROPRIEDADE: valor
+-- TEMPO: design
+-- EXPLICAÇÃO: false é uma escolha em tempo de design dos
+--desenvolvedores da LUA
+------------------------------------------------------------      
+
+METEOROS_ABATIDOS = 0
+
+
+local aviao_14bis={
+	abatido = false,
+	img = "imagens/nave.png",
+	largura = 55,
+	altura = 62,
+	x = 320/2 - 64/2,
+	y = 480 - 63,
+	tiros = {}
+
+}
+
+local function destroi_14bis()
+
+	destruicao:play()
+
+	aviao_14bis["img"] = love.graphics.newImage("imagens/explosao_nave.png")
+	aviao_14bis["largura"] = 67
+	aviao_14bis["altura"] = 77
+	
+	game_over:play()
+
+end
+
+
+
+
+
+local function move_14bis()
+
+	if love.keyboard.isDown("w") then
+		aviao_14bis["y"] = aviao_14bis["y"]-2
+	end	
+	if love.keyboard.isDown("s") then
+		aviao_14bis["y"] = aviao_14bis["y"]+2
+	end	
+	if love.keyboard.isDown("a") then
+		aviao_14bis["x"] = aviao_14bis["x"]-2
+	end	
+	if love.keyboard.isDown("d") then
+		aviao_14bis["x"] = aviao_14bis["x"]+2
+	end		
+ end
+
+ 
+
+local function da_tiro()
+	local tiro = {
+		-- tiro sai do meio do aviao
+		x= aviao_14bis["x"]+26,
+------------------------------------------------------------
+-- NOME: +
+-- PROPRIEDADE: semântica
+-- TEMPO: design
+-- EXPLICAÇÃO: O operador "+" usado para adição foi uma
+-- escolha em tempo de design da linguagem
+------------------------------------------------------------  
+
+
+		y= aviao_14bis["y"],
+		largura= 16,
+		altura = 16
+------------------------------------------------------------
+-- NOME: y
+-- PROPRIEDADE: valor
+-- TEMPO: execução
+-- EXPLICAÇÃO: o valor de y só será descoberto quando a função
+-- "da_tiro()" for chamada 
+------------------------------------------------------------ 		
+
+	}
+	shoot:play()
+	table.insert(aviao_14bis["tiros"],tiro)
+end
+
+local function move_tiros()
+	for key,tiro in pairs(aviao_14bis["tiros"]) do
+------------------------------------------------------------
+-- NOME: key e tiro
+-- PROPRIEDADE: endereço
+-- TEMPO: execução
+-- EXPLICAÇÃO: key e tiro são locais e seus endereços 
+-- só são determinados no tempo de execução do bloco for.
+------------------------------------------------------------   
+
+		if tiro["y"] >= 0 then
+			tiro["y"] = tiro["y"] -1
+		else 
+			-- remove tiro da memoria qdo sair da tela
+			table.remove(aviao_14bis["tiros"], key)
+		end	
+	end
+end
+
+local meteoros = {}
+
+
+
+local function cria_meteoros()
+	local meteoro = {
+		x= math.random(LARGURA_TELA),
+		y= -10,
+		peso = math.random(3),
+		deslocamento_lateral = math.random(-1,1),
+		largura = 50,
+		altura = 44
+		
+	}
+	table.insert(meteoros, meteoro)
+end
+
+local function move_meteoros()
+	for key,meteoro in pairs(meteoros) do
+		if meteoro["y"] <= ALTURA_TELA then
+			meteoro["y"] = meteoro["y"] + meteoro["peso"]
+			meteoro["x"] = meteoro["x"]+meteoro["deslocamento_lateral"]
+	
+		else 
+			-- remove meteoro da memoria qdo sair da tela
+			table.remove(meteoros, key)
+		end	
+	end
+end
+
+local function checa_colisao(x1,y1,L1,A1, x2,y2,L2,A2)
+
+	return 	x1 < x2+L2 and
+			x2 < x1+L1 and
+			y1 < y2+A2 and
+			y2 < y1+A1
+	
+end
+
+
+
+local function tem_colisao_com_aviao14bis()
+
+
+	for key,meteoro in pairs(meteoros) do
+		
+		if checa_colisao(meteoro["x"], meteoro["y"],meteoro["largura"], meteoro["altura"],
+						aviao_14bis["x"], aviao_14bis["y"],aviao_14bis["largura"], aviao_14bis["altura"]) then
+
+			
+			return true				
+
+		end	
+	end
+
+end	
+	
+local function checa_colisao_com_tiros()
+	
+	
+	for key_m,meteoro in pairs(meteoros) do
+		for key_t,tiro in pairs(aviao_14bis["tiros"]) do
+			if checa_colisao(meteoro["x"], meteoro["y"],meteoro["largura"], meteoro["altura"],
+							tiro["x"], tiro["y"],tiro["largura"], tiro["altura"]) then
+				
+				-- remove o tiro e o meteoro da tela				
+				table.remove(meteoros, key_m)
+				table.remove(aviao_14bis["tiros"],key_t)
+
+				
+		METEOROS_ABATIDOS = METEOROS_ABATIDOS+1
+	
+			end	
+		end
+	end
+
+end
+
+local function troca_musica_do_jogo_para(razao)
+	music:stop()
+
+	if razao == "FIM" then
+		game_over:play()
+	elseif razao == "VENCEDOR" then
+		winner:play()
+	end
+
+
+	
+end
+
+local function checa_colisoes()
+		
+	if tem_colisao_com_aviao14bis() then
+		destroi_14bis()
+		troca_musica_do_jogo_para("FIM")
+		FIM_JOGO = true
+	end	
+	checa_colisao_com_tiros() 
+end	
+
+local function checa_objetivo_concluido()
+	if METEOROS_ABATIDOS == 100 then
+		VENCEDOR = true
+		troca_musica_do_jogo_para("VENCEDOR")
+	end	
+end	
+
+function love.load()
+
+	love.window.setMode(320, 480, {resizable=false})
+	love.window.setTitle("14bis vs 100 meteoros")
+
+	math.randomseed(os.time())
+
+	background = love.graphics.newImage("imagens/background.png")
+	tiro_img = love.graphics.newImage("imagens/shoot.png")
+	meteoro_img = love.graphics.newImage("imagens/meteor.png")
+	gameover_img = love.graphics.newImage("imagens/gameover.png")
+	vencedor_img = love.graphics.newImage("imagens/vencedor.png")
+
+	music = love.audio.newSource("sons/music.wav")
+	music:setLooping(true)
+	music:play()
+
+	shoot = love.audio.newSource("sons/shoot.wav")
+	destruicao = love.audio.newSource("sons/bomba.wav")
+	game_over = love.audio.newSource("sons/game_over.wav")
+	winner = love.audio.newSource("sons/winner.wav")
+
+	aviao_14bis["img"] = love.graphics.newImage(aviao_14bis["img"])
+	
+end
+
+
+
+function love.update(dt)
+	if not FIM_JOGO and not VENCEDOR then
+		if love.keyboard.isDown('w', 'a', 's', 'd') then
+			move_14bis()
+		end
+		move_tiros()
+		if #meteoros < MAX_METEOROS_TELA then
+			cria_meteoros()
+		end
+		move_meteoros()
+		checa_colisoes()
+		checa_objetivo_concluido()	
+	end	
+			
+end
+
+function love.keypressed( key )
+	if key == "space" then
+	   da_tiro()
+	end
+ end
+
+
+function love.draw()
+	
+	love.graphics.draw(background,0,0)
+
+	for key,meteoro in pairs(meteoros) do
+		love.graphics.draw(meteoro_img, meteoro["x"], meteoro["y"])
+	end
+		
+	for key,tiro in pairs(aviao_14bis["tiros"]) do
+		love.graphics.draw(tiro_img, tiro["x"], tiro["y"])
+	end
+		
+	love.graphics.draw(aviao_14bis["img"], aviao_14bis["x"], aviao_14bis["y"])
+	love.graphics.print("Meteoros abatidos: ".. METEOROS_ABATIDOS, 0, 10)
+
+	if FIM_JOGO then
+		love.graphics.draw(gameover_img,LARGURA_TELA/2-100,ALTURA_TELA/2-100)
+	end
+
+	if VENCEDOR then
+		love.graphics.draw(vencedor_img,LARGURA_TELA/2-100,ALTURA_TELA/2-100)
+	end	
+		
+
+
+
+
+end
+
